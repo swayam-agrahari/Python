eeb248ef0c822cc8328742158db72b1ee015feb3
swayam-agrahari <109474492+swayam-agrahari@users.noreply.github.com>
2023-01-18 12:44:32 +0545

    Tip Calculator
    A python based project to calculate total amount including tip.

---
+++ tip_calculator.py
@@ -0,0 +1,7 @@
+print('Welcome to the tip calculator!!!')
+total=float(input('What was the total bill? $'))
+percent=int(input('What percentage tip would you like to give? 10, 12, or 15? '))
+person=int(input('How many people to split the bill? '))
+tip=float((total*percent)/(100*person))
+amount=tip+(total/person)
+print('Each person should pay: $'+str(round(amount,2)))
