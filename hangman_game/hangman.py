8018e2954e78395b0de4d4a2fb6291cbf1c483c5
swayam-agrahari <109474492+swayam-agrahari@users.noreply.github.com>
2023-01-20 23:56:06 +0545

    Hangman game in python

--- hangman_game/hangman.py
+++ hangman_game/hangman.py
@@ -1,21 +1,33 @@
 import random
-#word=['apple','ball','beautiful']
+# word=['apple','ball','beautiful']
 import module
-choosen_word= random.choice(module.word_list)
+print(module.logo)
+choosen_word = random.choice(module.word_list)
 print(f'The choosen word is {choosen_word}')
 display = []
 for char in choosen_word:
-    display+= "_"
+    display += "_"
 print(display)
 
-end_of_game= False
-while(not end_of_game):
-    word=input("Enter a word: ").lower() 
+end_of_game = False
+lives = 6
+while (not end_of_game):
+    word = input("Enter a word: ").lower()
+    if word in display:
+        print(f"You 've already guessed {word}")
     for position in range(len(choosen_word)):
-        if(word == choosen_word[position]):
-            display[position]= word
+        if (word == choosen_word[position]):
+            display[position] = word
+
     print(display)
     if '_' not in display:
-        end_of_game= True
+        end_of_game = True
         print('You win')
-
+    if word not in choosen_word:
+        print(f'you have guessed {word}, thats not in  the word!!! You lose a life!')
+        lives -=1
+        if lives == 0:
+            print("you lose")
+            end_of_game=True
+        
+        print(module.stages[lives])
