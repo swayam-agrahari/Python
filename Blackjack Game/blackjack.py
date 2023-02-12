50932aff263fc48467aa6ae2d62ec786eda7ecc4
swayam-agrahari <109474492+swayam-agrahari@users.noreply.github.com>
2023-02-12 08:29:32 +0545

    update

--- Blackjack Game/blackjack.py
+++ Blackjack Game/blackjack.py
@@ -1,14 +1,67 @@
-word = input("Do you want to play a game of Blackjack? Type 'y' or 'n':")
-cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
-
 import random
-def card(guess):
-    guess = random.choice(cards)
-    return guess
+word = input("Do you want to play a game of Blackjack? Type 'y' or 'n':")
 
-user_cards=[]
-computer_cards=[]
-for i in  range(2):
-    user_cards.append(card)
-    computer_cards.append(card)
-    
+
+def deal_card():
+    cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+    card = random.choice(cards)
+    return card
+
+
+user_cards = []
+computer_cards = []
+end_of_game = False
+
+for i in range(2):
+    user_cards.append(deal_card())
+    computer_cards.append(deal_card())
+
+
+def calculate_score(cards):
+    if sum(cards) == 21 and len(cards) == 2:
+        return 0
+
+    if 11 in cards:
+        if sum(cards) > 21:
+            cards.remove(11)
+            cards.append(1)
+    return sum(cards)
+
+
+def compare(my_score, computer_score):
+    if my_score == computer_score:
+        return "Draw"
+    elif computer_score == 0:
+        return "Lose, opponent has Blackjack"
+    elif my_score == 0:
+        return "Win with a Blackjack"
+    elif my_score == 21:
+        return "YOu went over. You lose"
+    elif computer_score > 21:
+        return "Opponent went over. You win"
+    elif my_score > computer_score:
+        return "You win "
+    else:
+        return "You lose "
+
+
+while (not end_of_game):
+    my_score = calculate_score(user_cards)
+    computer_score = calculate_score(computer_cards)
+
+    print(f'Your Cards {user_cards}, current score: {my_score}')
+    print(f"Computer's first card: {computer_cards[0]}")
+
+    if my_score == 0 or computer_score == 0 or my_score > 21:
+        end_of_game = True
+    else:
+        word = input("Type 'y' to get another card, type 'n' to pass:")
+        if (word == "y"):
+            user_cards.append(deal_card())
+            computer_cards.append(deal_card())
+        else:
+            end_of_game = True
+
+while (computer_score != 0 and computer_score > 17):
+    computer_cards.append(deal_card())
+computer_score = calculate_score(computer_cards)
